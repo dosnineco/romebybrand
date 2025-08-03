904e9bd7d3795f72e718b146723a695845b4922b
dosnineco <122121565+dosnineco@users.noreply.github.com>
2025-08-03 08:03:33 +0000

    out

--- pages/_app.js
+++ pages/_app.js
@@ -42,7 +42,7 @@ const isPublicRoute = isMarkdownPage || publicRoutes.some((route) =>
                     c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                     t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
                     y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                  })(window, document, "clarity", "script", "soy569oh8f");`,
+                  })(window, document, "clarity", "script", "soyei8vchm");`,
                 }}
               />
             )}
