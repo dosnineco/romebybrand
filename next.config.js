2445d22f967fb48e812a9e7907fe377db5740ac7
Tahjay Thompson <76020197+TahjayThompson@users.noreply.github.com>
2026-09-22 21:14:02 +0000

    out

--- next.config.js
+++ next.config.js
@@ -1,23 +1,26 @@
 const path = require('path');
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-});
-
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 });
 
-module.exports = withPWA(
-  withMDX({
-    reactStrictMode: true,
-    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-    images: {
-      domains: ['images.unsplash.com'],
-    },
-    webpack: (config) => {
-      config.resolve.alias['@components'] = path.join(__dirname, 'components');
-      return config;
-    },
-  })
-);
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+
+  images: {
+    domains: ['images.unsplash.com'],
+  },
+
+  webpack: (config) => {
+    config.resolve.alias['@components'] = path.join(
+      __dirname,
+      'components'
+    );
+
+    return config;
+  },
+};
+
+module.exports = withMDX(nextConfig);
